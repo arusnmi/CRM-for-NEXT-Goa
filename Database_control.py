eb8006789060ffc3c6d7fce8a696f022a7aef751
Arush_warty <parush.larty@gmail.com>
2026-02-09 11:25:14 +0530

    commit to hopefuly fix my shit

--- Database_control.py
+++ Database_control.py
@@ -1,1 +1,23 @@
-#Databaseing
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+credentials = Credentials.from_service_account_file("Creds.json", scopes=scopes)
+client = gspread.authorize(credentials)
+
+sheet_id = "1NaTDtP9V5mWVBKagHOQV2kADs9nMTk8YVJfXwriEFQU"
+
+def get_sheet_vals(sheet_id):
+    sheet=client.open_by_key(sheet_id)
+
+    value=sheet.sheet1.get_all_records()
+    return value
+
+
+def get_raw_data(value):
+    for i in range(len(value)):
+        mapvals= map(value[i].get, value[i])
+        print(list(mapvals))
+
+get_raw_data(get_sheet_vals(sheet_id))
--- from __future__ import print_function.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
-
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-
-credtiantls = service_account.Credentials.from_service_account_file('crm-system-481306-0a53d82b097a.json', scopes=SCOPES)
-sheet_service = build('sheets', 'v4', credentials=credtiantls)
-drive_service = build('drive', 'v3', credentials=credtiantls)
