7b9b9c9cad4a0e68db474e4c8f810c630625ade2
Arush_warty <parush.larty@gmail.com>
2026-03-23 13:49:28 +0530

    removed excisive vals from data contol

--- Database_control.py
+++ Database_control.py
@@ -22,7 +22,6 @@ def get_sheet_vals(sheet_id):
 
 
 def get_raw_data(value):
-    value=get_sheet_vals(sheet_id)
     VALS=[]
     for i in range(len(value)):
         mapvals= map(value[i].get, value[i])
